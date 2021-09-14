a11f6380c0173fd3e8611b63967fb97403c258da
Mateusz Kopij <sagasu@gmail.com>
2021-09-14 18:48:14 +0100

    forcasting price 10 days in the future and handling data that is not full - like nan fields

--- quandl_stocks.py
+++ quandl_stocks.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import quandl
+import math
 
 data_frame = quandl.get('WIKI/GOOGL')
 
@@ -12,3 +13,11 @@ data_frame['PCT_change'] = (data_frame['Adj. Close'] - data_frame['Adj. Open']) 
 data_frame = data_frame[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
 
 print(data_frame)
+
+forecast_col = 'Adj. Close'
+data_frame.fillna(-99999, inplace=True)
+
+forecast_out = int(math.ceil(0.1*len(data_frame)))
+data_frame['label'] = data_frame[forecast_col].shift(-forecast_out)
+data_frame.dropna(inplace=True)
+print(data_frame.head())
